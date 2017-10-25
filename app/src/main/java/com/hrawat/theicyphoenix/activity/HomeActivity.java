4cd157ad7a70a91fc30f60892639ba1a98044db4
hrawat_xavient <harshrock2010@gmail.com>
2017-10-25 14:28:51 +0530

    code cleanup

--- app/src/main/java/com/hrawat/theicyphoenix/activity/HomeActivity.java
+++ app/src/main/java/com/hrawat/theicyphoenix/activity/HomeActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +20,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hrawat.theicyphoenix.R;
 import com.hrawat.theicyphoenix.activity.utils.BitmapUtils;
@@ -60,6 +60,8 @@ public class HomeActivity extends AppCompatActivity
             public void onClick(View view) {
                 if (editText.getText().length() > 0)
                     startCovertingImage();
+                else
+                    Toast.makeText(HomeActivity.this, "No text found!!!", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -146,26 +148,6 @@ public class HomeActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
